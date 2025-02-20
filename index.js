f7ed7d15d829479005473cab9c9f18f97e501786
MrXAlone1 <mr.xpro8315@gmail.com>
2025-02-20 12:39:10 +0530

    test html file1

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 import express from 'express'
 // import dotenv  from 'dotenv'
 import { config } from 'dotenv'
+import path from 'path'
 
 const  app = express()
+
 // dotenv.config({path:'./.env'})
 config({path:'./.env'})
 const PORT = process.env.PORT
-console.log(PORT)
+console.log(path)
+
+
+// __dirname setting
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
 
+
+app.get('/add',(req,res)=>{
+    console.log(__dirname,path.join(__dirname,'./public/welcome'))
+    res.sendFile(path.join(__dirname,'./public/home.html'))
+})
+
 app.get('/home',(req,res)=>{
-    res.render('home.html')
+    res.sendFile(path.join(__dirname,'../home.html'))
 })
 
 app.get('/alone3000',(req,res)=>{
